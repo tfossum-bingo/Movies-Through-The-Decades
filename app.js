d2aef6b349576afb1c62cb86740e5f9060116e40
Ted Fossum <theodorefossum@gmail.com>
2020-10-19 14:05:00 -0400

    Return primary movie title to the page.

--- app.js
+++ app.js
@@ -2,12 +2,43 @@ console.log('Connected!')
 
 let myList = {}
 let primaryMovie
+let candidateMovies
+let myDocument = document.querySelector('body')
 
 const DOMAIN = 'http://www.omdbapi.com/';
 const API_KEY = '3279c95f'
 const BASE_URL = `${DOMAIN}?apikey=${API_KEY}&s=`;
-
 console.log('BASE_URL: ', BASE_URL)
+
+
+const createPrimaryMovieTile = (movie) => {
+    const new_div = newDiv()
+    new_div.className = 'primary_movie'
+    new_div.appendChild(createH2Title(movie))
+    new_div.appendChild(createULDetails(movie))
+    console.log(new_div)
+    return new_div
+}
+
+const createH2Title = (movie) => {
+    const new_h2 = document.createElement('h2')
+    new_h2.innerText = movie['Title']
+    console.log('H2 Title: ', new_h2)
+    return new_h2
+}
+
+const createULDetails = (movie) => {
+    const new_ul = document.createElement('ul')
+    const desired_keys = ['Year']
+    desired_keys.forEach(element => {
+        const new_li = document.createElement('li')
+        new_li.innerHTML = movie[element]
+        new_ul.appendChild.new_li
+    })
+    console.log('New_UL: ', new_ul)
+    return new_ul
+}
+
 
 const getMovies = async (titleSearch) => {
     try{
@@ -20,16 +51,27 @@ const getMovies = async (titleSearch) => {
     }
 }
 
-const processList = (movie_list) => {
-    const sortedList = sortMovies(movie_list)
-    setPrimaryMovie(sortedList)
+const findOlderMovies = (sorted_movies, years_ago) => {
+    const olderMovies = sorted_movies.filter(movie => movie['Year'] < primaryMovie['Year'] - years_ago)
+    console.log('Older Movies', olderMovies)
 }
 
-const setPrimaryMovie = (sorted_movies) => {
+const findPrimaryMovie = (sorted_movies) => {
     if(sorted_movies.length > 0){
-        primaryMovie = sorted_movies[0]
+        primaryMovie = sorted_movies.shift()
         console.log('Primary Movie: ', primaryMovie)
     }
+}
+
+const newDiv = () => {
+    return document.createElement('div')
+}
+
+const processList = (movie_list) => {
+    const sortedList = sortMovies(movie_list)
+    findPrimaryMovie(sortedList)
+    findOlderMovies(sortedList, 10)
+    myDocument.appendChild(createPrimaryMovieTile(primaryMovie))
 }
 
 const sortMovies = (movie_list) => {
@@ -40,5 +82,6 @@ const sortMovies = (movie_list) => {
     return movie_list
 }
 
-console.log(getMovies('heat'))
+console.log(getMovies('fast'))
 console.log('myList: ', myList)
+
